a042c123a47c8281cb07c9a57706043d4bf6b79c
Eric Himmelblau <erichimmelblau@gmail.com>
2020-01-15 00:28:54 -0600

    Add id key to blogPost object, add delete functionality to BlogContext and IndexScreen.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,20 +1,22 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const { state, addBlogPost } = useContext(BlogContext);
+    const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return <View>
         <Button title="Add Post" onPress={addBlogPost} />
         <FlatList 
             data={state}
-            keyExtractor={(blogPost) => blogPost.title}
+            keyExtractor={(blogPost) => blogPost.id.toString()}
             renderItem={({ item }) => {
                 return <View style={styles.row}>
-                    <Text style={styles.title}>{item.title}</Text>
-                    <Feather style={styles.icon} name="trash" />
+                    <Text style={styles.title}>{item.title} - {item.id}</Text>
+                    <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                        <Feather style={styles.icon} name="trash" />
+                    </TouchableOpacity>
                 </View>
             }}
         />
