a762f305ac0bb58b071794681bc4a331d3a1e3c9
Eric Himmelblau <erichimmelblau@gmail.com>
2020-01-16 01:09:27 -0600

    Add EditScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,43 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
+
+const EditScreen = ({ navigation }) => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(BlogContext);
+
+    return <View>
+        <Text style={styles.label}>Enter Title:</Text>
+        <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
+        <Text style={styles.label}>Enter Content:</Text>
+        <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
+        <Button 
+            title="Add Blog Post"
+            onPress={() => {
+                addBlogPost(title, content, () => {
+                    navigation.navigate('Index');
+                });
+            }} 
+        />
+    </View>
+};
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        padding: 5,
+        margin: 5,
+        marginBottom: 15
+
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5
+    }
+});
+
+export default EditScreen;
