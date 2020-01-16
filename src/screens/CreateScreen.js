a448d66398eed81e1cad40a08905444e76a21f99
Eric Himmelblau <erichimmelblau@gmail.com>
2020-01-16 00:55:05 -0600

    Add content feild to blogpost and wire up create screen to BlogContext

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,13 +5,21 @@ import { Context as BlogContext } from '../context/BlogContext';
 const CreateScreen = ({ navigation }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(BlogContext);
 
     return <View>
         <Text style={styles.label}>Enter Title:</Text>
         <TextInput style={styles.input} value={title} onChangeText={(text) => setTitle(text)} />
         <Text style={styles.label}>Enter Content:</Text>
         <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)} />
-        <Button title="Add Blog Post" />
+        <Button 
+            title="Add Blog Post"
+            onPress={() => {
+                addBlogPost(title, content, () => {
+                    navigation.navigate('Index');
+                });
+            }} 
+        />
     </View>
 };
 
